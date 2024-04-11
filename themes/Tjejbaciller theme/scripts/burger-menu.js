82ea012b2783a95409533f4be5f83b5dbc2442c9
henrietta jeansson <henriettajeansson@gmail.com>
2024-04-11 11:19:15 +0200

    update jQuery functions with overlay div

--- themes/Tjejbaciller theme/scripts/burger-menu.js
+++ themes/Tjejbaciller theme/scripts/burger-menu.js
@@ -1,17 +1,29 @@
 /** @format */
 
 jQuery(document).ready(function () {
+  // Function to turn on the overlay
+  function on() {
+    jQuery(".close-nav").css("display", "block");
+  }
+
+  // Function to turn off the overlay
+  function off() {
+    jQuery(".close-nav").css("display", "none");
+  }
+
+  // Open menu
   jQuery(".toggle-nav").click(function (e) {
     jQuery("#menu-primary-menu").slideToggle(500);
     jQuery(".close-nav").addClass("active-close");
-
+    on(); // Turn on overlay when menu opens
     e.preventDefault();
   });
-  //Close
+
+  // Close menu
   jQuery(".close-nav").click(function (e) {
     jQuery("#menu-primary-menu").slideToggle(500);
     jQuery(".close-nav").removeClass("active-close");
-
+    off(); // Turn off overlay when menu closes
     e.preventDefault();
   });
 });
